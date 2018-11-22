a961d7ba16a3a0a5ff6d1ee0046649bc8acb2011
liweihao <1766141548@qq.com>
2018-11-22 11:47:07 +0800

    implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #自动生成一个ｓｅｓｓｉｏｎ辅助模块，　而且，　其中的辅助方法会自动引入到
+  # 试图中，　如果载控制器的基类中引入辅助方法，　还可以载控制器中　使用
+  include SessionsHelper
 end
