37d13810f323d01f6dd85b4b31a4b5225980acb2
liweihao <1766141548@qq.com>
2018-11-22 11:10:09 +0800

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  def show
+    begin
+      @user = User.find(params[:id])
+    rescue
+      @user = false
+    end
+    # debugger
+    if !!@user
+       @user
+    else
+      render html:'没有此用户'
+    end
+  end
+  def index
+  end
+  def delete
+    @user = User.find(params[:id])
+    if !!@user
+      @user.delete
+    else
+      render html:'没有此用户'
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name,  :email, "password", :password_confirmation)
   end
 end
